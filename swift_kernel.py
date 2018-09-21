8a6e7dafdf4621077ca959b748042a39102f7863
Marc Rasi <marcrasi@google.com>
2018-09-20 22:16:50 -0700

    hmm

--- swift_kernel.py
+++ swift_kernel.py
@@ -502,6 +502,6 @@ class SwiftKernel(Kernel):
         }
 
 if __name__ == '__main__':
-    print("hello world")
+    print("hello world 2")
     from ipykernel.kernelapp import IPKernelApp
     IPKernelApp.launch_instance(kernel_class=SwiftKernel)
