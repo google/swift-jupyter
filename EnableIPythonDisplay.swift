50ce4f348f76f4f146293275a1e58e93ae7f79f9
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2019-05-25 02:28:40 +0530

    Update argument label for display function

--- EnableIPythonDisplay.swift
+++ EnableIPythonDisplay.swift
@@ -84,6 +84,6 @@ IPythonDisplay.enable()
 func display(base64EncodedPNG: String) {
   let displayImage = Python.import("IPython.display")
   let codecs = Python.import("codecs")
-  let imageData = codecs.decode(Python.bytes(base64Png, encoding: "utf8"), encoding: "base64")
+  let imageData = codecs.decode(Python.bytes(base64EncodedPNG, encoding: "utf8"), encoding: "base64")
   displayImage.Image(data: imageData, format: "png").display()
 }
